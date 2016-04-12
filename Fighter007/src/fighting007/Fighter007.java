71640cbb718e173fb553f1acf2e91a3b36bf8ce0
RaziNanu <raziyeh.ghassemi@st.ovgu.de>
2016-04-12 18:10:03 +0200

    Add Folder

---
+++ Fighter007/src/fighting007/Fighter007.java
@@ -0,0 +1,46 @@
+package fighting007;
+
+import gameInterface.AIInterface;
+import structs.FrameData;
+import structs.GameData;
+import structs.Key;
+
+public class Fighter007 implements AIInterface {
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getCharacter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getInformation(FrameData arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int initialize(GameData arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Key input() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processing() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
